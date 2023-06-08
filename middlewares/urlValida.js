1ea86be0a8463c9cbfe110c06db57f9b72daad83
carlosedu1234 <65097149+carlosedu1234@users.noreply.github.com>
2023-06-07 21:12:32 -0500

    validaciones urls

--- middlewares/urlValida.js
+++ middlewares/urlValida.js
@@ -1,6 +1,29 @@
-const urlValidar=(req,res,next)=>{
+const path = require('path');
+const { URL } = require('url');
+const urlValidar = (req, res, next) => {
 
+    try {
 
+        const { origin } = req.body;
+        const urlFrontEnd = new URL(origin);
+
+        if (urlFrontEnd.origin !== "null") {
+            return next();
+
+        }
+        else { throw new Error("no valida 😤"); }
+
+    }
+
+    catch (e) {
+
+    
+        return res.sendFile(path.join(__dirname,'../img/gato-entonces.gif'));
+        
+       
+    
+
+    }
 
 
 };
@@ -9,4 +32,4 @@ const urlValidar=(req,res,next)=>{
 
 
 
-module.exports = urlValida;
+module.exports = urlValidar;
--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,9 @@ const express=require('express');
 const router=express.Router();
 const { leerUrls,agregarUrl,eliminarUrl } =require('../controller/homeController');
 
-
+const urlValida =require('../middlewares/urlValida');
 router.get('/',leerUrls);
-router.post('/',agregarUrl); 
+router.post('/',urlValida,agregarUrl); 
 router.get('/eliminar/:id',eliminarUrl);
 
 module.exports =router;
